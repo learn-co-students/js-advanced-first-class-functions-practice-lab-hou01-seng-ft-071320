2bb73c82b79eb2a9658208efbf746790df9c6dbf
JuanToanXoup <Johndoan42@gmail.com>
2020-08-25 01:04:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function logDriverNames(driver){
+    driver.forEach(function(obj){
+        console.log(obj['name'])
+        }
+    )
+}
+
+function logDriversByHometown(driver,location){
+    logDriverNames(driver.filter(function(obj){return obj['hometown'] === location}))
+}
+
+function driversByRevenue(driver){
+    return [...driver].sort(function(obj1,obj2){
+        return obj1['revenue'] - obj2['revenue']
+        }
+    )
+}
+
+function driversByName(driver){
+    return [...driver].sort(function(obj1,obj2){
+        return obj1['name'].localeCompare(obj2['name']);
+        }
+    )
+}
+
+function totalRevenue(driver){
+    return [...driver].map(function(obj){return obj['revenue']}).reduce((accumlator, currentValue)  => {return accumlator + currentValue;},0);
+}
+
+function averageRevenue(driver){
+    return totalRevenue(driver) / driver.length
+}
